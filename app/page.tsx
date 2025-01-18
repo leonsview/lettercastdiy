2351c4583304df6c1a2391021f970ea42d2be4a1
Leon Sandner <mail@leonsandner.com>
2025-01-18 12:46:28 +0100

    clerk installed

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+"use server"
+
+export default async function HomePage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">lettercast.fyi</h1>
+    </div>
+  )
+}
